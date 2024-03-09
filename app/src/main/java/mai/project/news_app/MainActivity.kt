813a0c6ee3c0b09a5a321f128c16224ed1e93f03
DDPlay123 <a70325000@gmail.com>
2024-03-09 13:48:17 +0800

    feat: migrate systemBar color setting to edgeToEdge api

--- app/src/main/java/mai/project/news_app/MainActivity.kt
+++ app/src/main/java/mai/project/news_app/MainActivity.kt
@@ -3,13 +3,14 @@ package mai.project.news_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -26,23 +27,19 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        WindowCompat.setDecorFitsSystemWindows(window, false)
+        // 設置系統 UI 延伸到屏幕邊緣，現遷移到 edgeToEdge API
+//        setupSystemUIFitWindow()
+        // 先顯示啟動畫面
         installSplashScreen().apply {
             // 保持啟動畫面顯示條件，直到 ViewModel 中的 splashCondition 變為 false
             setKeepOnScreenCondition { viewModel.splashCondition }
         }
+        // 啟用 edgeToEdge API，使 UI 延伸到屏幕邊緣
+        enableEdgeToEdge()
         setContent {
             NewsAppTheme {
-                // 用於實現系統狀態欄和導航欄的完全透明
-                val isSystemInDarkTheme = isSystemInDarkTheme()
-                val systemController = rememberSystemUiController()
-                // 重組時，根據系統主題切換系統狀態欄和導航欄的顏色
-                SideEffect {
-                    systemController.setSystemBarsColor(
-                        color = Color.Transparent,
-                        darkIcons = !isSystemInDarkTheme
-                    )
-                }
+                // 用於實現系統狀態欄和導航欄的完全透明，現遷移到 edgeToEdge API
+//                SetupSystemBarsColorWithAccompanist()
                 // 主畫面
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -52,6 +49,36 @@ class MainActivity : ComponentActivity() {
                     NavGraph(startDestination = startDestination)
                 }
             }
+        }
+    }
+
+    /**
+     * 用於延伸到屏幕邊緣
+     *
+     * - false: 不需要自動調整佈局，使 UI 延伸到屏幕邊緣
+     * - 用於搭配 [SetupSystemBarsColorWithAccompanist] 一起使用
+     */
+    private fun setupSystemUIFitWindow() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+    }
+
+    /**
+     * 用於實現系統狀態欄和導航欄的完全透明
+     *
+     * - 使用 Accompanist Library
+     * @see <a href="https://google.github.io/accompanist/systemuicontroller/">Accompanist</a>
+     */
+    @Composable
+    private fun SetupSystemBarsColorWithAccompanist() {
+        // 用於實現系統狀態欄和導航欄的完全透明
+        val isSystemInDarkTheme = isSystemInDarkTheme()
+        val systemController = rememberSystemUiController()
+        // 重組時，根據系統主題切換系統狀態欄和導航欄的顏色
+        LaunchedEffect(isSystemInDarkTheme, systemController) {
+            systemController.setSystemBarsColor(
+                color = Color.Transparent,
+                darkIcons = !isSystemInDarkTheme
+            )
         }
     }
 }
--- app/src/main/java/mai/project/news_app/ui/theme/Theme.kt
+++ app/src/main/java/mai/project/news_app/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package mai.project.news_app.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,12 +8,8 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     background = Black,
@@ -38,25 +33,18 @@ fun NewsAppTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-      dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-        val context = LocalContext.current
-        if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-      }
-      darkTheme -> DarkColorScheme
-      else -> LightColorScheme
-    }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-      SideEffect {
-        val window = (view.context as Activity).window
-        window.statusBarColor = colorScheme.primary.toArgb()
-        WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-      }
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
     }
 
     MaterialTheme(
-      colorScheme = colorScheme,
-      typography = Typography,
-      content = content
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
     )
 }
